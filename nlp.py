3f0fca9ce7be38e20cb389c10ad1f81ea115ddbe
Tim Konle <tim.konle@web.de>
2022-05-12 23:22:41 +0200

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,23 +1,16 @@
-import token
-import nltk
-from nltk.stem import PorterStemmer
 import spacy
+from spacy.lang.de.stop_words import STOP_WORDS
 
-ps = PorterStemmer()
+
+
 nlp = spacy.load('de_core_news_md')
 
-def tokenize(sentence):
+def preprocess(sentence):
     doc = nlp(sentence)
 
     lemma_tokens = []
     for token in doc:
-        lemma_tokens.append(token.lemma_.lower())
+        if token.is_stop == False and token.is_punct == False:
+            lemma_tokens.append(token.lemma_.lower())
     
-    return lemma_tokens
-def main():
-    Eingabe = "urlaub beurlauben!"
-
-    print(tokenize(Eingabe))
-
-if __name__ == "__main__":
-    main()
+    return ' '.join(lemma_tokens)
